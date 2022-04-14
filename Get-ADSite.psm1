6fe917cf72ccbe110cfd68fae10bcdf2540f48ce
Travis Knight <travis.knight@macys.com>
2022-04-14 15:25:38 -0400

    v1.5.4.2 Update Get-ADSite

--- Get-ADSite.psm1
+++ Get-ADSite.psm1
@@ -1,48 +1,49 @@
+#Requires -Modules ActiveDirectory
 <#
 .SYNOPSIS
     Module for returning AD Site details.
 .DESCRIPTION
     Use this module to perform search of a whole or partial AD site name or subnet
     or to get the AD site of a computer.
-.PARAMETER ComputerName 
+.PARAMETER ComputerName
     Enter the full name or IP address of a computer.
 .PARAMETER Full
 	A switch will return the full AD site info.  Used in conjuntion with ComputerName value.
 .PARAMETER Name
 	This parameter is a full or partial AD site name.
-.PARAMETER Subnet 
+.PARAMETER Subnet
     Enter the first two or three octets of an IP address.  Subnets are treated as regular expressions;
     all rules pertaining to regular expressions are in play.
 .PARAMETER All
     A switch that will simply get ALL known sites and subnets. This is helpful when assessing large datasets.
     No other parameter will be assessed when this switch is true.
-.EXAMPLE 
+.EXAMPLE
     Get-ADSite -ComputerName "D1233210"
 
     ComputerName Site
     ------------ ----
     D1233210     Duluth
 
-    
-    
+
+
     Description
     -----------
     Returns the Site of the computer input.
-.EXAMPLE 
+.EXAMPLE
     Get-ADSite -ComputerName "D1233210" -Full
 
     Site      Description     Subnet
     ----      -----------     ------
     DULUTH    Duluth, GA      11.166.240.0/24, 11.166.244.0/23
 
-    
-    
+
+
     Description
     -----------
     Used only in conjuntion with ComputerName value.
-    Use of this switch returns the Site, Description and Subnet(s) of 
+    Use of this switch returns the Site, Description and Subnet(s) of
     the identified AD Site of the computer input.
-.EXAMPLE 
+.EXAMPLE
     Get-ADSite -Name "Duluth"
 
     Site      Description     Subnet
@@ -53,9 +54,9 @@
 
     Description
     -----------
-    This example of a full or partial AD site name returns the Site, Description and 
+    This example of a full or partial AD site name returns the Site, Description and
     Subnet(s) of locations matching the name input.
-.EXAMPLE 
+.EXAMPLE
     Get-ADSite -Subnet "11.48"
 
     Site      Description     Subnet
@@ -75,6 +76,7 @@
 	Date: 2018-05-25: tmknight: Updated help messages
 	Date: 2018-08-22: tmknight: Add switch to pull all known AD Sites and subnets
 	Date: 2018-11-21: tmknight: Add Test-SubnetMember to assess boundary membership
+	Date: 2022-04-14: tmknight: Switch to Get-ADReplicationSite/Subnet from ActiveDirectory module
 .NOTES
    Note: Please enter only one of an AD Site Name, a portion of the subnet address a computer name or the All switch.
    Using more than one parameter is not supported at this time.
@@ -99,15 +101,18 @@ function  Get-ADSite {
         [Parameter(ParameterSetName = "p2",
             Mandatory = $false)]
         [string] $Subnet,
+        [Parameter(ParameterSetName = "p2",
+            Mandatory = $false)]
+        [string] $Domain = (Get-CimInstance -ClassName Win32_ComputerSystem).Domain,
         [Parameter(ParameterSetName = "p3",
             HelpMessage = "A switch that will simply get ALL known sites and subnets. No other parameter will be assessed",
             Mandatory = $false)]
         [switch] $All
     )
-    
+
     begin {
         $ErrorActionPreference = "Stop"
-        
+
         if ($All) {
             ## A switch that will simply get ALL known sites and subnets. No other parameter will be assessed
         }
@@ -129,100 +134,22 @@ function  Get-ADSite {
         }
     }
     process {
-        function  get-stats {
-            param(
-                $Subnet,
-                $Name,
-                [switch]$All
-            )
-
-            $rslt = @()
-            $siteDescription = @{}
-            $siteSubnets = @{}
-            
-            $sitesDN = "LDAP: / / CN = Sites, " + $([adsi] "LDAP://RootDSE").Get("ConfigurationNamingContext")
-            $subnetsDN = "LDAP: / / CN = Subnets, CN = Sites, " + $([adsi] "LDAP://RootDSE").Get("ConfigurationNamingContext")
-            ## Get all the site names and descriptions
-            foreach ($site in $([adsi] $sitesDN).psbase.children) {
-                if ($site.objectClass -eq "site") {
-                    $siteName = ([string]$site.cn).toUpper()
-                    $siteDescription[$siteName] = $site.Description
-                    $siteSubnets[$siteName] = @()
-                }
-            }
-
-            ## Get all subnets and associate them with the sites
-            foreach ($net in $([adsi] $subnetsDN).psbase.children) {
-                $site = [adsi] "LDAP: / / $($net.siteObject)"
-                if ($site.cn -ne $null) {
-                    $siteName = ([string]$site.cn).toUpper()
-                    $siteSubnets[$siteName] += $net.cn
-                }
-                else {
-                    $siteDescription["Orphaned"] = "Subnets not associated with any site"
-                    if ($siteSubnets["Orphaned"] -eq $null) { 
-                        $siteSubnets["Orphaned"] = @() 
-                    }
-                    $siteSubnets["Orphaned"] += $net.cn
-                }
-            }
-
-            ## Gather results
-            foreach ($siteName in $siteDescription.keys) {
-                ## Site info based on subnet parameter
-                if ($Subnet -or $All) {
-                    foreach ($net in $siteSubnets[$siteName] | Sort-Object) {
-                        if ($All) {
-                            $rslt += [PSCustomObject] @{
-                                Site        = $siteName
-                                Description = $($siteDescription[$siteName])
-                                Subnet      = $net
-                            }
-                        }
-                        elseif ($net -match "\b$Subnet") {
-                            $rslt += [PSCustomObject] @{
-                                Site        = $siteName
-                                Description = $($siteDescription[$siteName])
-                                Subnet      = $net
-                            }
-                        }
-                        elseif (Test-SubnetMember -Subnet $Subnet -Boundary $net) {
-                            $rslt += [PSCustomObject] @{
-                                Site        = $siteName
-                                Description = $($siteDescription[$siteName])
-                                Subnet      = $net
-                            }
-                        }
-                    }
-                }
-                ## Site info based on name parameter
-                elseif ($siteName -match "$Name") {
-                    $net = $siteSubnets[$siteName] -join ", "
-                    $rslt += [PSCustomObject] @{
-                        Site        = $siteName
-                        Description = $($siteDescription[$siteName])
-                        Subnet      = $net
-                    }
-                }
-            }
-            return $rslt
-        }        
-
         ## get AD site of a specific computer
         switch ($PsCmdlet.ParameterSetName) {
             "p0" {
                 try {
                     if ($ComputerName -eq $env:COMPUTERNAME) {
-                        $siteName = (Get-CimInstance -ClassName Win32_NTDomain -Filter "Name = 'Domain: FEDERATED'").ClientSiteName
+                        $cn = "."
                     }
                     else {
-                        $siteName = (Get-CimInstance -ComputerName $ComputerName -ClassName Win32_NTDomain -Filter "Name = 'Domain: FEDERATED'").ClientSiteName
+                        $cn = $ComputerName
                     }
+                    $siteName = (Get-CimInstance -ComputerName $cn -ClassName Win32_NTDomain | Where-Object { $Domain -Match $_.DomainName -and $null -ne $_.ClientSiteName }).ClientSiteName
                 }
                 catch {
                     $siteName = ($_.Exception).Message
                 }
-                
+
                 if ($siteName -eq $err -or ($siteName -and $Full -ne $true)) {
                     $result = [PSCustomObject] @{
                         ComputerName = $ComputerName
@@ -230,17 +157,49 @@ function  Get-ADSite {
                     }
                 }
                 elseif ($siteName -ne $err -and $Full) {
-                    $result = get-stats -Name $siteName
+                    $Name = $siteName
+                    $site = Get-ADReplicationSite -Identity $Name
+                    $subs = Get-ADReplicationSubnet -Filter "Site -eq '$($site.DistinguishedName)'"
+                    $result = [PSCustomObject]@{
+                        Site        = $site.Name
+                        Description = $site.Description
+                        Subnet      = $subs.Name -join "; "
+                    }
                 }
             }
             "p1" {
-                $result = get-stats -Name $Name
+                $site = Get-ADReplicationSite -Identity $Name
+                $subs = Get-ADReplicationSubnet -Filter "Site -eq '$($site.DistinguishedName)'"
+                $result = [PSCustomObject]@{
+                    Site        = $site.Name
+                    Description = $site.Description
+                    Subnet      = $subs.Name -join "; "
+                }
             }
             "p2" {
-                $result = get-stats -Subnet $Subnet
+                $result = @()
+                $subs = Get-ADReplicationSubnet -Filter "Name -like '$Subnet*'"
+                foreach ($item in $subs) {
+                    $site = Get-ADReplicationSite -Filter * | Where-Object DistinguishedName -EQ "$($item.site)"
+                    $result += [PSCustomObject]@{
+                        Site        = $site.Name
+                        Description = $site.Description
+                        Subnet      = $item.Name -join "; "
+                    }
+                }
+
             }
             "p3" {
-                $result = get-stats -All
+                $result = @()
+                $site = Get-ADReplicationSite -Filter *
+                foreach ($item in $site) {
+                    $subs = Get-ADReplicationSubnet -Filter "Site -eq '$($item.DistinguishedName)'"
+                    $result += [PSCustomObject]@{
+                        Site        = $item.Name
+                        Description = $item.Description
+                        Subnet      = $subs.Name -join "; "
+                    }
+                }
             }
         }
     }
--- TMK-CoreModules.psd1
+++ TMK-CoreModules.psd1
@@ -2,7 +2,7 @@
 # Module manifest for shared modules'
 ###
 @{
-    ModuleVersion     = '1.5.4.1'
+    ModuleVersion     = '1.5.4.2'
     GUID              = '0c83f152-41a4-4997-92da-c06923fb3e12'
     Author            = 'Travis M Knight'
     CompanyName       = 'TMK World Headquarters'
@@ -74,7 +74,8 @@
             * 2019-11-22: tmknight: v1.5.3.15: Write-InlineProgress: Number format to zero places.  Fix assessment of $PSHOME to match windows directory path format
             * 2021-02-12: tmknight: v1.5.3.16: Start-Fast-Ping: Correct Port parameter to be optional; no default value
             * 2021-03-16: tmknight: v1.5.4.0: New commandlet Get-ADGroupMembership
-            * 2021-03-16: tmknight: v1.5.4.1: Start-MultiThreading code cleanup'
+            * 2021-03-16: tmknight: v1.5.4.1: Start-MultiThreading code cleanup
+            * 2022-04-14: tmknight: v1.5.4.2: Update Get-ADSite to use ActiveDirectory module'
         }
     }
 }
